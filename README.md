72b19634a3ee8fc05045edac1fc1a2a9f35ed79e
Ron <ron_rose@me.com>
2023-05-24 13:53:17 -0400

    updated amiiboattack

--- README.md
+++ README.md
@@ -1,17 +1,16 @@
-# Basketball-Blitz!
-A website for basketball fans to gather and research data.
+# Amiibo-Attack!
+A website for amiibo fans to gather and research data.
 
 
-Basketball Blitz is a site where basketball connoisseurs come to challenge their knowledge  about the National Basketball Association and its players / coaches.  Users have the ability to search any active player and their statistics while also having the option to compare that player to another. 
+Amiibo Attack is a site where amiibo connoisseurs come to challenge their knowledge  about various Nintendo Amiibos and their origin.  Users have the ability to search amiibos and view their origin date / game series while also having the option to compare that amiibo to another. 
 
-Using HTML, CSS, vanilla JavaScript, and a public API I will construct the website using a search bar to get input with an abstract button that is encapsulated and also inherits properties that will cause the object to change its form making it polymorphic! q^_^p “alley-OOP!”
+Using HTML, CSS, vanilla JavaScript, and a public API I will construct the website using a search bar to get input with an abstract button that is encapsulated and also inherits properties that will cause the object to change its form making it polymorphic! 
 
-**TRIVIA GAME: random facts**
-**livefeed of games with stats?**
 
-- In HTML: place 2 search bars / buttons (input for each player) in their own divs to GET data and also display results once pulled (player photo and stats)
+
+- In HTML: place 2 search bars / buttons (input for each amiibo) in their own divs to GET data and also display results once pulled (amiibo photo and info)
 - In CSS: use styles to give the site a clean and crisp look. 
-- In JS: use NBA API to make calls pulling the requested data and display it on site by referencing specific elements in HTML
+- In JS: use Amiibo API to make calls pulling the requested data and display it on site by referencing specific elements in HTML
 
 
 Trello (whiteboard): https://trello.com/b/CV4UFyJ1/intro-to-trello
--- script.js
+++ script.js
@@ -1,43 +1,106 @@
 const button1 = document.querySelector("#searchButton1")
 
 button1.addEventListener('click', async () => {
-    // const apiKey = '78ad7a7a7f7569aaea70c6a492b24c64'
-
-    let plyrName1 = document.querySelector("#plyrName1")
+  
     
+
+
+    let plyr1Name = document.querySelector("#plyr1Name")
+    let plyr1Photo1 = document.querySelector("#plyr1Photo1")
+    let plyr1Series1 = document.querySelector('#plyr1Series1')
+    let plyr1Release1 = document.querySelector('#plyr1Release1')
+    let plyr1Photo2 = document.querySelector('#plyr1Photo2')
+    let plyr1Series2 = document.querySelector('#plyr1Series2')
+    let plyr1Release2 = document.querySelector('#plyr1Release2')
+    
+
     let input = document.querySelector('#inputBar1')
 
 
     let person1 = input.value
         
-  
-    let response1 = await axios.get(`https://www.balldontlie.io/api/v1/players/${person1}`)
+    let response1 = await axios.get(`https://www.amiiboapi.com/api/amiibo/?name=${person1}`)
+        
+    if (response1.data.amiibo.length < 2){
+        plyr1Name.innerHTML = `Name: ${response1.data.amiibo[0].name}`
+        plyr1Photo1.innerHTML = `<img alt="An image of ${response1.data.amiibo[0].name}." src="${response1.data.amiibo[0].image}"/>`
+        plyr1Series1.innerHTML = `Game Series: ${response1.data.amiibo[0].gameSeries}`
+        plyr1Release1.innerHTML= `Amiibo Release Date: ${response1.data.amiibo[0].release.na}`
+
+
+        plyr1Series2.style.display = 'none'
+        plyr1Release2.style.display = 'none'
+        plyr1Photo2.style.display = 'none'
     
-    plyrName1.innerHTML = `Name: ${response1.data.first_name}`
+        }else {}
+    plyr1Name.innerHTML = `Name: ${response1.data.amiibo[0].name}`
+    plyr1Photo1.innerHTML = `<img alt="An image of ${response1.data.amiibo[0].name}." src="${response1.data.amiibo[0].image}"/>`
+    plyr1Series1.innerHTML = `Game Series: ${response1.data.amiibo[0].gameSeries}`
+    plyr1Release1.innerHTML= `Amiibo Release Date: ${response1.data.amiibo[0].release.na}`
+    plyr1Photo2.innerHTML = `<img alt="An image of ${response1.data.amiibo[0].name}." src="${response1.data.amiibo[1].image}"/>`
+    plyr1Series2.innerHTML = `Game Series: ${response1.data.amiibo[1].gameSeries}`
+    plyr1Release2.innerHTML = `Amiibo Release Date: ${response1.data.amiibo[1].release.na}`
+
+    plyr1Series2.style.display = 'block'
+    plyr1Release2.style.display = 'inline'
+    plyr1Photo2.style.display = 'inline'
+
     
     console.log(response1.data)
-    })
+})
 
 
 
 
 
-    const button2 = document.querySelector("#searchButton2")
+const button2 = document.querySelector("#searchButton2")
 
-    button2.addEventListener('click', async () => {
-        // const apiKey = '78ad7a7a7f7569aaea70c6a492b24c64'
-    
-        let plyrName2 = document.querySelector("#plyrName2")
+button2.addEventListener('click', async () => {
         
-        let input = document.querySelector('#inputBar2')
     
+    let plyr2Name = document.querySelector("#plyr2Name")
+    let plyr2Photo1 = document.querySelector("#plyr2Photo1")
+    let plyr2Series1 = document.querySelector('#plyr2Series1')
+    let plyr2Release1 = document.querySelector('#plyr2Release1')
+    let plyr2Photo2 = document.querySelector('#plyr2Photo2')
+    let plyr2Series2 = document.querySelector('#plyr2Series2')
+    let plyr2Release2 = document.querySelector('#plyr2Release2')
     
-        let person2 = input.value
+    let input = document.querySelector('#inputBar2')
+
+
+    let person2 = input.value
+        
+    
+    let response2 = await axios.get(`https://www.amiiboapi.com/api/amiibo/?name=${person2}`)
+    
+    if (response2.data.amiibo.length < 2){
+        plyr2Name.innerHTML = `Name: ${response2.data.amiibo[0].name}`
+        plyr2Photo1.innerHTML = `<img alt="An image of ${response2.data.amiibo[0].name}." src="${response2.data.amiibo[0].image}"/>`
+        plyr2Series1.innerHTML = `Game Series: ${response2.data.amiibo[0].gameSeries}`
+        plyr2Release1.innerHTML= `Amiibo Release Date: ${response2.data.amiibo[0].release.na}`
+        
+
+        plyr2Series2.style.display = 'none'
+        plyr2Release2.style.display = 'none'
+        plyr2Photo2.style.display = 'none'
+
+        }else {
+            plyr2Name.innerHTML = `Name: ${response2.data.amiibo[0].name}`
+            plyr2Photo1.innerHTML = `<img alt="An image of ${response2.data.amiibo[0].name}." src="${response2.data.amiibo[0].image}"/>`
+            plyr2Series1.innerHTML = `Game Series: ${response2.data.amiibo[0].gameSeries}`
+            plyr2Release1.innerHTML= `Amiibo Release Date: ${response2.data.amiibo[0].release.na}`
+            plyr2Photo2.innerHTML = `<img alt="An image of ${response2.data.amiibo[0].name}." src="${response2.data.amiibo[1].image}"/>`
+            plyr2Series2.innerHTML = `Game Series: ${response2.data.amiibo[1].gameSeries}`
+            plyr2Release2.innerHTML = `Amiibo Release Date: ${response2.data.amiibo[1].release.na}`
             
-      
-        let response2 = await axios.get(`https://www.balldontlie.io/api/v1/players/${person2}`)
+            plyr2Series2.style.display = 'block'
+            plyr2Release2.style.display = 'inline'
+            plyr2Photo2.style.display = 'inline'
+            
+
+    }
         
-        plyrName2.innerHTML = `Name: ${response2.data.first_name}`
-        
-        console.log(response2.data)
-        })
+       
+    console.log(response2.data)
+})
